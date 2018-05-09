cf5a662a062a5a462b20d6a1b78ebb30503e4e46
Isaac Shoman and Matt Miller <matt@>
2018-05-09 10:55:57 -0700

    Display list of pre-loaded projects from firebase

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { ProjectService } from '../project.service';
+import { Project } from '../project.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.css']
+  styleUrls: ['./project-list.component.css'],
+  providers: [ProjectService]
 })
+
 export class ProjectListComponent implements OnInit {
   projects: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private projectService: ProjectService) {}
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
 }
